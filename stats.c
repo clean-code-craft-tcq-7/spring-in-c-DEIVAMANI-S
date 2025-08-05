85d518efe871880b14bcd8ecb8d86a488d7f5a60
qei1cob <qei1cob@bosch.com>
2025-08-05 13:05:38 +0530

    TDD Statistics - Fix for single NaN data
    TDD Statistics - Code Fix for single NaN data in list

--- stats.c
+++ stats.c
@@ -8,6 +8,11 @@ struct Stats compute_statistics(const float* numbers, int count) {
     stats.min = FLT_MAX;
     stats.max = -FLT_MAX;
     for (int i = 0; i < count; i++) {
+
+        //One of the value in batch is invalid
+        if(isnan(numbers[i])) {
+            continue; // ignore NaN values
+        }
         //Calculates min, max, avereage values based on Numbers provided for statistics 
         if (numbers[i] < stats.min) {
             stats.min = numbers[i];
